ccec421aed60696503a4740d158e67a361b88311
Jonathan Hunter <hunterjonathant@gmail.com>
2017-08-31 03:45:11 -0400

    Basic DartThrowing

---
+++ Assets/Scripts/DartThrowing/CircleDart.cs
@@ -0,0 +1,27 @@
+﻿namespace GraphicsResearch.DartThrowing
+{
+    using UnityEngine;
+
+    public class CircleDart : MonoBehaviour
+    {
+        [SerializeField]
+        private float radius;
+
+        public float Radius { get { return this.radius; } set { this.radius = value; } }
+
+        public bool IsOverlapping(CircleDart circle)
+        {
+            Vector3 edge = Vector3.MoveTowards(circle.transform.position, this.transform.position, circle.Radius);
+            float dist = Vector3.Distance(edge, this.transform.position);
+            if (dist < this.Radius)
+                return true;
+
+            edge = Vector3.MoveTowards(this.transform.position, circle.transform.position, this.Radius);
+            dist = Vector3.Distance(edge, circle.transform.position);
+            if (dist < circle.Radius)
+                return true;
+
+            return false;
+        }
+    }
+}
---
+++ Assets/Scripts/DartThrowing/DartThrower.cs
@@ -0,0 +1,82 @@
+﻿namespace GraphicsResearch.DartThrowing
+{
+    using System.Collections.Generic;
+    using UnityEngine;
+
+    public class DartThrower : MonoBehaviour
+    {
+        [SerializeField]
+        private Transform center;
+        [SerializeField]
+        private Vector2 dimensions;
+        [SerializeField]
+        private int numberOfDarts;
+        [SerializeField]
+        private int numberOfAttempts;
+        [SerializeField]
+        private float circleRadius;
+        [SerializeField]
+        private CircleDart circleTemplet;
+
+        private float upBound;
+        private float downBound;
+        private float leftBound;
+        private float rightBound;
+        private List<CircleDart> circles;
+
+        private void Start()
+        {
+            this.upBound = center.position.y + dimensions.y / 2;
+            this.downBound = center.position.y - dimensions.y / 2;
+            this.leftBound = center.position.x - dimensions.x / 2;
+            this.rightBound = center.position.x + dimensions.x / 2;
+            this.circles = new List<CircleDart>();
+
+            for(int i = 0; i < numberOfDarts; i++)
+            {
+                ThrowDart();
+            }
+        }
+
+        private void Update()
+        {
+            if(Input.GetKeyUp(KeyCode.C))
+            {
+                ThrowDart();
+            }
+        }
+
+        private void ThrowDart()
+        {
+            CircleDart circle = Instantiate<CircleDart>(this.circleTemplet);
+            circle.transform.localScale = Vector3.one * this.circleRadius * 2f;
+            circle.Radius = this.circleRadius;
+            int currentAttempt = 0;
+            while(currentAttempt < this.numberOfAttempts)
+            {
+                float x = Random.Range(leftBound, rightBound);
+                float y = Random.Range(downBound, upBound);
+                circle.transform.position = new Vector2(x, y);
+                bool overlaps = false;
+                foreach(CircleDart cd in this.circles)
+                {
+                    if(cd.IsOverlapping(circle))
+                    {
+                        overlaps = true;
+                    }
+                }
+
+                if(!overlaps)
+                {
+                    this.circles.Add(circle);
+                    return;
+                }
+
+                currentAttempt++;
+            }
+
+            Debug.Log("Unable to place dart after " + currentAttempt + " attempts.");
+            Destroy(circle.gameObject);
+        }
+    }
+}
